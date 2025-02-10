dfcb6d18b8bb00b588a3d1a5e00769f1abdd891f
mmamorim <marc.m.amorim@gmail.com>
2025-02-10 11:49:50 -0300

    v2

--- Aula01-10-Fev/main.js
+++ Aula01-10-Fev/main.js
@@ -1,8 +1,27 @@
 
 console.log("oi gente");
 
-let i = 1
-while(i<10) {
-    console.log(i);
-    i++
+//let i = 1
+//while(i<10) {
+//    console.log(i);
+//    i++
+//}
+
+let elem = document.getElementById("botao")
+console.log(elem);
+
+elem.addEventListener("click",jogoDoPIM)
+
+function jogoDoPIM() {
+    let elem = document.getElementById("caixa")
+    let qtde = parseInt(elem.value)
+    let texto = ""
+    for (let i = 1; i < qtde; i++) {
+        if (i % 4 == 0) {
+            texto = texto + "PIM "
+        } else {
+            texto = texto + i + ","
+        }
+    }
+    console.log(texto);
 }

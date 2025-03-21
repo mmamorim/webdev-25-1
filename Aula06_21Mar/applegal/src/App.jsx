b2584a803ea8413513ac95993b72cd6e89cec0b0
mmamorim <marc.m.amorim@gmail.com>
2025-03-21 09:49:05 -0300

    v1

--- Aula06_21Mar/applegal/src/App.jsx
+++ Aula06_21Mar/applegal/src/App.jsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <>
       <h1>Meu app legal</h1>
+
+      <span>Alguma coisa</span>
+      <span className="border border-cyan-800 rounded px-1 bg-blue-200 hover:bg-blue-600 hover:border-3 cursor-pointer mx-2">
+        Outra coisa
+      </span>
     </>
   )
 }
